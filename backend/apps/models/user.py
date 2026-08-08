77ad84aabbd1a8ba313b53dd925fd766547a25bc
Ayush Rai <ayush.veersa@gmail.com>
2026-08-08 14:18:11 +0000

    user model and schema

---
+++ backend/apps/models/user.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Integer, String, Column, DateTime, Boolean, Enum
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from apps.db.database import Base
+from backend.apps.schemas.role import Role
+
+
+class User(Base):
+    __tablename__="users"
+
+    id=Column(Integer, nullable=False, primary_key=True, index=True, autoincrement=True)
+    name=Column(String(100), nullable=False)
+    email=Column(String(255), unique=True, index=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    age=Column(Integer, nullable=False)
+    is_active=Column(Boolean, default=True, nullable=False)
+    role = Column(Enum(Role, name="user_role"), nullable=False, default=Role.EMPLOYEE)
+    employee = relationship(
+            "Employee",
+            back_populates="user",
+            uselist=False,
+    )
+    created_at=Column(DateTime(
+            timezone=True),
+            server_default=func.now()
+    )
+    updated_at=Column(DateTime(
+            timezone=True),
+            default=func.now(),
+            onupdate=func.now()
+    )
---
+++ backend/apps/schemas/user.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+
+from backend.apps.schemas.role import Role
+
+
+class UserRegister(BaseModel):
+    #id: int
+    name: str = Field(min_length=1, max_length=100)
+    password: str
+    email: EmailStr
+    age: int
+    role: Role
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    age: int
+    role: Role
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str="bearer"
+    user: UserResponse
